48bb6679fc8cd40b9a98022e0883b4ec5d7a30f9
Chanon Roy <chanonr@surveymonkey.com>
2018-08-30 12:10:45 -0400

    Aspect ratio settings

--- src/js/CroppingTool.jsx
+++ src/js/CroppingTool.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Cropper from 'cropperjs';
+import { Button } from 'element-react';
 
 class CroppingTool extends React.Component {
     constructor(props) {
@@ -19,13 +20,17 @@ class CroppingTool extends React.Component {
                 <div className="cropper-preview">
                     <img id="imagePreview" src={this.state.image} />
                 </div>
-                <div className="upload-btn">
-                    <input id="upload"
-                        className="upload-btn__input"
+                <div className="upload-img">
+                    <label htmlFor="upload-photo" className="el-button el-button--primary">
+                        Upload
+                    </label>
+                    <input id="upload-photo"
+                        className="upload-img__input"
                         accept=".jpg,.png"
                         type="file"
                         onChange={(e) => this.handleUpload(e)}
-                        name="img-upload" />
+                        name="photo" />
+                    <Button> Clear </Button>
                 </div>
             </div>
         )
@@ -46,11 +51,12 @@ class CroppingTool extends React.Component {
     initializeCropper() {
         const image = document.getElementById('imagePreview');
         const cropper = new Cropper(image, {
+            aspectRatio: 1 / 1,
             cropBoxResizable: false,
-            minContainerWidth: 300,
-            minContainerHeight: 300,
+            minCanvasHeight: 300,
             minCanvasWidth: 300,
-            minCanvasHeight: 300
+            minContainerHeight: 300,
+            minContainerWidth: 300
         });
 
     }
